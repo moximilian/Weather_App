0947255363bc42a095f34ef48d716a2ff1f2b9ab
Maxim Syrov <surovmax03@gmail.com>
2023-08-07 21:56:23 +0300

    all fine

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ export default function App() {
   return <>
     <Router>
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/about' element={<About />} />
+        <Route path='https://moximilian.github.io/Weather_App/' element={<Main />} />
+        <Route path='https://moximilian.github.io/Weather_App/about' element={<About />} />
       </Routes>
     </Router>
   </>
